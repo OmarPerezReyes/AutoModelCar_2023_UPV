e0488bfa04d20242e8d8b2462ea84e53b189bcb2
OmarPerezReyes <2130073@upv.edu.mx>
2023-04-19 20:18:19 +0200

    Add package Frame Capture
    Save an frame from the topic

---
+++ catkin_ws/src/hardware/frame_capture/src/frame_capture_node.cpp
@@ -0,0 +1,36 @@
+#include <librealsense2/rs.hpp>
+#include <signal.h>
+#include "ros/ros.h"
+#include "sensor_msgs/Image.h"
+
+#include <opencv2/opencv.hpp>
+
+
+void imageCallback(const sensor_msgs::Image::ConstPtr& msg)    
+{
+    cv::Mat color(cv::Size(640, 480), CV_8UC3, (unsigned char*)msg->data.data(), cv::Mat::AUTO_STEP);
+    int key = cv::waitKey(1);
+    if(key==32){
+
+        cv::imwrite("/home/ros/source_code/Assets/frame.jpg", color);   
+        std::cout<<"Frame saved"<<std::endl;
+        exit(1);        
+    }
+
+    cv::imshow("img", color);
+    cv::waitKey(1);
+}
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "frame_capture_node");
+
+    ros::NodeHandle n;
+
+    ros::Subscriber sub = n.subscribe("/realsense/color_raw", 1000, imageCallback);
+    std::cout<<"Clic space to save"<<std::endl;
+
+    ros::spin(); 
+
+    return 0;
+
+}
