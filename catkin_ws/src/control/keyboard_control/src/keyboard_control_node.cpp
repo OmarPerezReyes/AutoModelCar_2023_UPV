2c4eef9efe0e468d366d3939537023349784add8
OmarPerezReyes <2130073@upv.edu.mx>
2023-04-25 19:54:42 +0200

    Optimization in Keyboard Control

--- catkin_ws/src/control/keyboard_control/src/keyboard_control_node.cpp
+++ catkin_ws/src/control/keyboard_control/src/keyboard_control_node.cpp
@@ -14,137 +14,96 @@
 #define KEYCODE_S 0x73
 #define KEYCODE_D 0x64
 
-bool die = false;
-bool first_time = true;
-
 ros::Publisher pubSpeed;
 ros::Publisher pubSteering;
 autominy_msgs::SpeedPWMCommand speedMsg;
 autominy_msgs::SteeringPWMCommand steeringMsg;
 
+int speed = 0;
+int steering = 1500;
+bool die = false;
 
-int keyControl(ros::Rate r){
-    int kfd;
-    int speed;
-    int steering;
-   
+void publishSpeedMsg() {
+    speedMsg.value = speed;
+    pubSpeed.publish(speedMsg);
+}
+
+void publishSteeringMsg() {
+    steeringMsg.value = steering;
+    pubSteering.publish(steeringMsg);
+}
+
+void initializeKeyboard() {
+    int kfd = 0;
     char c;
     struct termios cooked, raw;
+    // get the console in raw mode
+    tcgetattr(kfd, &cooked);
+    memcpy(&raw, &cooked, sizeof(struct termios));
+    raw.c_lflag &=~ (ICANON | ECHO);
+    raw.c_cc[VEOL] = 1;
+    raw.c_cc[VEOF] = 2;
+    tcsetattr(kfd, TCSANOW, &raw);
 
+    ROS_INFO("Reading from keyboard");
+    ROS_INFO("---------------------------");
+    ROS_INFO("Moving around:");
+    ROS_INFO("       w    ");
+    ROS_INFO("   a   s   d");
+    ROS_INFO("---------------------------");
+}
 
-    if(first_time){
-
-        speed=0;
-        steering=1500;
-
-        speedMsg.value = speed;
-        pubSpeed.publish(speedMsg);
-
-
-        steeringMsg.value = steering;
-        pubSteering.publish(steeringMsg);
-        kfd = 0;
-        // get the console in raw mode
-        tcgetattr(kfd, &cooked);
-        memcpy(&raw, &cooked, sizeof(struct termios));
-        raw.c_lflag &=~ (ICANON | ECHO);
-        raw.c_cc[VEOL] = 1;
-        raw.c_cc[VEOF] = 2;
-        tcsetattr(kfd, TCSANOW, &raw);
-
-        puts("Reading from keyboard");
-        puts("---------------------------");
-        puts("Moving around:");
-        puts("       w    ");
-        puts("   a   s   d");
-        puts("---------------------------");
-
-        first_time=false;	
-    }
-
-    while(ros::ok() && !die){
-      
-        if(read(kfd, &c, 1) < 0)
-        {
+void keyControl(ros::Rate r) {
+    initializeKeyboard();
+    char c;
+    int kfd = 0;
+    while (ros::ok() && !die) {
+        if (read(kfd, &c, 1) < 0) {
             perror("read():");
             exit(-1);
         }
-
-        switch(c)  
-        {
+        switch (c) {
             case KEYCODE_W:
-            if (speed > -100 && speed < 100){
-                if (speed < 0){  
-                    speed = 0;
-                    speedMsg.value = speed;
-                    pubSpeed.publish(speedMsg);
+                if (speed < 100) {
+                    speed += 10;
                 }
-                pubSpeed.publish(speedMsg);
-                speed += 10;
-                speedMsg.value = speed;
-                pubSpeed.publish(speedMsg);
-            }
-            break;
-
+                publishSpeedMsg();
+                break;
             case KEYCODE_S:
                 speed = 0;
-                speedMsg.value = speed;
-                pubSpeed.publish(speedMsg);
+                publishSpeedMsg();
                 break;
-
             case KEYCODE_A:
-                if (steering > 1000){
-                    steering -= 100.0;  
-                    steeringMsg.value = steering;
-                    pubSteering.publish(steeringMsg);
-                }        
-            break;
-
+                if (steering > 1000) {
+                    steering -= 100;
+                }
+                publishSteeringMsg();
+                break;
             case KEYCODE_D:
-                if (steering < 2000){
-                steering += 100.0;  
-                steeringMsg.value = steering;
-                pubSteering.publish(steeringMsg);
-                } 
-            break;
+                if (steering < 2000) {
+                    steering += 100;
+                }
+                publishSteeringMsg();
+                break;
             default:
-                   die=true;
+                die = true;
+                break;
         }
-
-        r.sleep();     
+        r.sleep();
     }
-
-    return 0;
-
 }
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char* argv[]) {
     ros::init(argc, argv, "manual_control_node");
     ros::NodeHandle n;
-    
-    ros::Rate r(50); // 10 hz
-  
+    ros::Rate r(50); // 50 Hz
     pubSpeed = n.advertise<autominy_msgs::SpeedPWMCommand>("/actuators/speed_pwm", 10);
     pubSteering = n.advertise<autominy_msgs::SteeringPWMCommand>("/actuators/steering_pwm", 10);
-
     keyControl(r);
- 
     speedMsg.value = 0;
+    publishSpeedMsg();
     steeringMsg.value = 1500;
-
-    pubSteering.publish(steeringMsg);
-
-    r.sleep();   
-    r.sleep();   
-    r.sleep();   
-    r.sleep();       
-
-    pubSpeed.publish(speedMsg);
-
-    r.sleep();       
-
+    publishSteeringMsg();
     ros::shutdown();
-   
-    // ros::spin();
+    return 0;
 }
